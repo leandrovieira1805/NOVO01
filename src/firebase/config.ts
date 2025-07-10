3dcedddf8565259b7c1a7c21582d00c7ab97ecba
Seu Nome <seu@email.com>
2025-07-10 07:15:10 -0300

    Corrige config do Firebase com databaseURL

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,16 +1,18 @@
-import { initializeApp } from 'firebase/app';
-import { getDatabase } from 'firebase/database';
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
 
+// Configuração do Firebase com databaseURL
 const firebaseConfig = {
-  apiKey: "SUA_API_KEY_AQUI",
-  authDomain: "seu-projeto.firebaseapp.com",
-  databaseURL: "https://seu-projeto-default-rtdb.firebaseio.com",
-  projectId: "seu-projeto",
-  storageBucket: "seu-projeto.appspot.com",
-  messagingSenderId: "123456789",
-  appId: "1:123456789:web:abcdef123456"
+  apiKey: "AIzaSyAuZ1SJWxlwWtgVhV3qnBafoytho59WE4I",
+  authDomain: "device-streaming-77144326.firebaseapp.com",
+  databaseURL: "https://device-streaming-77144326-default-rtdb.firebaseio.com", // <-- Adicione esta linha!
+  projectId: "device-streaming-77144326",
+  storageBucket: "device-streaming-77144326.appspot.com",
+  messagingSenderId: "375948005973",
+  appId: "1:375948005973:web:99b7ff4736d6c17f927adc"
 };
 
+// Inicializa o Firebase
 const app = initializeApp(firebaseConfig);
 export const database = getDatabase(app);
 export default app; 
