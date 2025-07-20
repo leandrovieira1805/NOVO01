d2fd532bc36d52a7c720acdd0890f2cb7ea66cb4
Seu Nome <seu@email.com>
2025-07-20 08:48:45 -0300

    fix: remover uso de MenuProvider do App.jsx e corrigir rotas

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,21 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { MenuProvider } from './context/MenuContext';
 import { CartProvider } from './context/CartContext';
 import CustomerView from './components/CustomerView';
 import AdminLogin from './components/AdminLogin';
 import AdminPanel from './components/AdminPanel';
 import './App.css';
 
-function App() {
+export default function App() {
   return (
-    <MenuProvider>
+    <Router>
       <CartProvider>
-        <Router>
-          <div className="App">
-            <Routes>
-              <Route path="/" element={<CustomerView />} />
-              <Route path="/admin" element={<AdminLogin />} />
-              <Route path="/admin/panel" element={<AdminPanel />} />
-            </Routes>
-          </div>
-        </Router>
+        <Routes>
+          <Route path="/" element={<CustomerView />} />
+          <Route path="/admin/login" element={<AdminLogin />} />
+          <Route path="/admin" element={<AdminPanel />} />
+        </Routes>
       </CartProvider>
-    </MenuProvider>
+    </Router>
   );
 }
-
-export default App;
